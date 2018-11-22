c05e23d8ba59c872774ab66d1b9ded6f6f64fa6a
williamovero <orgertricewill@gmail.com>
2018-11-22 04:18:09 -0500

    First commit

---
+++ polls/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
+# Create your views here.
+
+def index(request):
+    template = loader.get_template('')
+    #return HttpResponse('')
+    return render(request, 'index.html')
